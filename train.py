8d0ab69db88a13360a5ebd3e390b89c6f7067d38
vchilk <chilakamarrivivek@gmail.com>
2022-05-10 02:02:41 -0500

    working training harness for aggression net

---
+++ train.py
@@ -0,0 +1,39 @@
+# from .models import ClassificationLoss, model_factory, save_model
+from utils import load_data
+from model import MLPClassifier, save_model
+from os import path
+import torch
+from torch.utils.tensorboard import SummaryWriter
+
+def train():
+    device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
+    model = MLPClassifier().to(device)
+    epochs = 50
+    train_logger = SummaryWriter(path.join('./log', 'train'), flush_secs=1)
+    """
+    Your code here
+
+    """
+    data = load_data()
+    optimizer = torch.optim.Adam(model.parameters(), lr=1e-3)
+    loss_function = torch.nn.MSELoss()
+    global_step = 0
+    for epoch in range(epochs):
+        print("Epoch #: ", epoch)
+        for state, label in data:
+            state = state.type(torch.FloatTensor).to(device)
+            label = label.type(torch.FloatTensor).to(device)
+            logit = model(state).reshape((-1, 2, 5))
+            
+            loss = loss_function(logit, label)
+            train_logger.add_scalar('loss', loss, global_step)
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+            
+            global_step += 1
+    save_model(model)
+
+
+if __name__ == '__main__':
+    train()
---
+++ utils.py
@@ -0,0 +1,33 @@
+
+import numpy as np
+from torch.utils.data import Dataset, DataLoader
+from torchvision import transforms
+import torch
+import csv
+
+
+class CMetricDataset(Dataset):
+    def __init__(self):
+        self.data = np.load('observations.npy')
+        self.labels = np.load('labels.npy')
+
+    def __len__(self):
+        """
+        Your code here
+        """
+        return len(self.data)
+
+    def __getitem__(self, idx):
+        """
+        Your code here
+        return a tuple: img, label
+        """
+        t = transforms.ToTensor()
+        obsv = torch.tensor(self.data[idx])
+        label = torch.tensor(self.labels[idx])
+        return((torch.squeeze(obsv), torch.squeeze(label)))
+
+
+def load_data(num_workers=0, batch_size=32):
+    dataset = CMetricDataset()
+    return DataLoader(dataset, num_workers=num_workers, batch_size=batch_size, shuffle=True, drop_last=False)
